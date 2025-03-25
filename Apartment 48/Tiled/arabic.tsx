d7ebd0a1bad5df4b499b13fccd93596c8267f96a
Silivver <pesterevsasha35@gmail.com>
2025-03-25 03:54:24 +0300

    Add files via upload

---
+++ Apartment 48/Tiled/arabic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="arabic" tilewidth="12" tileheight="12" tilecount="672" columns="21">
+ <image source="arabic.png" width="256" height="384"/>
+</tileset>
---
+++ Apartment 48/Tiled/jawbreaker_sample.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="jawbreaker_sample" tilewidth="12" tileheight="12" tilecount="441" columns="21">
+ <image source="../jawbreaker_sample.png" width="256" height="256"/>
+</tileset>
